90f8ef9ad0ea5efdbe3d8e865cbd45a9bb7b9f83
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-11-12 12:16:30 -0500

    Completed Project
    Test how many math questions you can answer in under a minute. 3 difficulties - Easy, Medium, Hard. Easy uses single digit number equations, medium uses double digit number equations and hard uses tripe digit number equations. All numbers and equations are randomly generated. Answer correctly to gain points and answer incorrectly to lose. Game was tied to a DB to track leaderboard and top 10 scoring players. Different difficulties gained different points

---
+++ MathApp/BusinessLogic/LeaderboardBL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathApp.Models;
+using DataLibrary.DTOs;
+using DataLibrary.Repositories;
+
+namespace MathApp.BusinessLogic
+{
+    public class LeaderboardBL
+    {
+        public void AddNewLeaderboard(LeaderboardModel leaderboardModel)
+        {
+            //Convert model to DTO
+            LeaderboardDTO leaderboardDTO = ConvertLeaderboardToDTO(leaderboardModel);
+            //Write new record to DB
+            LeaderboardRepository leaderboardRepository = new LeaderboardRepository();
+            leaderboardRepository.AddToLeaderboard(leaderboardDTO);
+        }
+        public List<LeaderboardModel> GetLeaderboard()
+        {
+            List<LeaderboardModel> leaderboardModels = new List<LeaderboardModel>();
+            LeaderboardRepository leaderboardRepository = new LeaderboardRepository();
+
+            List<LeaderboardDTO> leaderboardDTOs = leaderboardRepository.GetLeaderboard();
+            if (leaderboardDTOs.Count > 0)
+            {
+                foreach (LeaderboardDTO leaderboardDTO in leaderboardDTOs)
+                {
+                    LeaderboardModel lm = new LeaderboardModel();
+                    lm.Username = leaderboardDTO.Username;
+                    lm.score = leaderboardDTO.Score;
+
+                    leaderboardModels.Add(lm);
+                }
+            }
+            return leaderboardModels;
+        }
+        public LeaderboardDTO ConvertLeaderboardToDTO(LeaderboardModel lm)
+        {
+            LeaderboardDTO leaderboardInformation = new LeaderboardDTO();
+            leaderboardInformation.Username = lm.Username;
+            leaderboardInformation.Score = lm.score;
+
+            return leaderboardInformation;
+        }
+    }
+}
---
+++ MathApp/Models/LeaderboardModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApp.Models
+{
+    public class LeaderboardModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public int score { get; set; }
+    }
+}
